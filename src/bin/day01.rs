4ab95e6cd86d0ffcc174fbb00731e5c6bd213562
Weber Chu <weberchu@amazon.com>
2024-11-30 17:48:19 +0000

    Prepare for 2024

---
+++ src/bin/day01.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("../../input/day01.txt");
+    part1(input);
+    part2(input);
+}
+
+fn part1(input: &str) {
+    println!("part 1: {}", input);
+}
+
+fn part2(input: &str) {
+    println!("part 2: {}", input);
+}
+
